4beba0607bf1501b7b0df6faa4d5e193bda1373f
Matt Wrock <matt@mattwrock.com>
2016-03-05 11:53:05 -0800

    move path sub to test

--- test/cookbooks/machine_test/recipes/default.rb
+++ test/cookbooks/machine_test/recipes/default.rb
@@ -1,5 +1,4 @@
 location = File.join(ENV['LOCALAPPDATA'] || '/tmp', 'machine')
-location.sub!('appveyor', ENV['machine_user'])
 
 directory location do
   action [:delete, :create]
--- test/integration/default/default_spec.rb
+++ test/integration/default/default_spec.rb
@@ -1,4 +1,5 @@
 location = File.join(ENV['LOCALAPPDATA'] || '/tmp', 'machine')
+location.sub!('appveyor', ENV['machine_user'])
 
 describe directory(location) do
   it { should exist }
